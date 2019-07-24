2c3a49ad295a936cf37d31944f2517aca160f445
John42506176Linux <johnkongtcheu@gmail.com>
2019-07-24 14:21:47 -0400

    Comment and like functionality has been added

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from webapp2_extras.auth import InvalidPasswordError
 from google.appengine.api import images
 from Login import *
 from posts import *
+from Comment import *
 # the handler section
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -41,11 +42,11 @@ class MainPage(BaseHandler, blobstore_handlers.BlobstoreUploadHandler):
         tag2_inputted = self.request.get("tag2")
         tag3_inputted = self.request.get("tag3")
 
-        post = Post(tag1 = tag1_inputted, tag2 = tag2_inputted, tag3 = tag3_inputted)
+        post = Post(tag1 = tag1_inputted, tag2 = tag2_inputted, tag3 = tag3_inputted,user = self.user.key,user_name = self.user.username)
         post.image = upload.key()
         post.image_url = images.get_serving_url(post.image)
 
-        query=Post.query()
+        query = Post.query(Post.user_name == self.user.username)
         all_posts=query.fetch()
         all_posts.append(post)
         post.put()
@@ -55,6 +56,38 @@ class MainPage(BaseHandler, blobstore_handlers.BlobstoreUploadHandler):
         }
         upload_url = blobstore.create_upload_url('/')
         self.response.out.write(template.render("templates/home.html", template_vars).format(upload_url))
+class LikePost(BaseHandler):
+    @user_required
+    def post(self):
+        post_key = self.request.get("post")
+        post = Post.get_by_id(int(post_key))
+        post.likes +=1
+        post.put()
+
+class CommentPostPage(BaseHandler):
+    @user_required
+    def post(self):
+        comment_content = self.request.get("comment")
+        print(comment_content)
+        post_key = self.request.get("post")
+        print(post_key)
+        post = Post.get_by_id(int(post_key))
+        comment = Comment(user=self.user.key, user_name =self.user.username,content=comment_content)
+        comment.put()
+        post.comments.append(comment)
+        post.put()
+        upload_url = blobstore.create_upload_url('/')
+        query = Post.query(Post.user_name == self.user.username)
+        all_posts = query.fetch()
+        template_vars = {
+            "all_posts": all_posts,
+        }
+        self.response.out.write(template.render("templates/home.html", template_vars).format(upload_url))
+
+
+
+
+
 # the app configuration section
 config = {
   'webapp2_extras.auth': {
@@ -70,4 +103,6 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/Logout', handler=LogOutPage, name='logout'),
     webapp2.Route('/Login', handler=LoginPage, name='login'),
     webapp2.Route('/sign_up', handler=SignUpPage, name='SignUp'),
+    webapp2.Route('/comment', handler=CommentPostPage, name='CommentPost'),
+    webapp2.Route('/Like', handler=LikePost, name='LikePost'),
 ], debug=True,config=config)
--- posts.py
+++ posts.py
@@ -1,12 +1,18 @@
 from google.appengine.ext import ndb
 from google.appengine.api import images
-
+from Users import User
+from Comment import Comment
 class Post (ndb.Model):
     image = ndb.BlobKeyProperty()
+    user =  ndb.KeyProperty(User)
+    user_name = ndb.StringProperty()
     image_url = ndb.StringProperty()
     tag1 = ndb.StringProperty()
     tag2 = ndb.StringProperty()
     tag3 = ndb.StringProperty()
+    comments = ndb.StructuredProperty(Comment,repeated=True)
+    likes = ndb.IntegerProperty(default = 0)
 
     def create_post_photo_url(self):
         return images.get_serving_url(self.image)
+
